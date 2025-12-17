acf7eed252a725464e280543a80d6d5f92b7e664
Leonardo Tomaz <99373384+tmazleo@users.noreply.github.com>
2025-12-17 16:42:36 -0300

    Remove hardcoded access token from main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 		log.Fatal("Erro ao carregar .env")
 	}
 
-	accessToken := "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL2FwaS5yZC5zZXJ2aWNlcyIsInN1YiI6InRsanByZEJUbGtUazZfaXZ3QmJXeDhkWW1HUXd2RnJ6XzRER0V5Y2pLYk1AY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vYXBwLnJkc3RhdGlvbi5jb20uYnIvYXBpL3YyLyIsImFwcF9uYW1lIjoiQkkgLSBDb250cm9sIEY1IiwiZXhwIjoxNzY2MDAwMDcxLCJpYXQiOjE3NjU5MTM2NzEsInNjb3BlIjoiIn0.DqPd9tmwCXwp6VgffDfC0f82IA9-tauCbyIm8T5ugU_Uz07dDGs49VYq_ds6r7tfqyXnUzi0anEr7va_tT890oQ0JZnP7d13aN6gvracjiQCrvuBaYXEx72Xn4OFnX95ZB7n6nEs1y3U9p46jRPssm1zR9ENDjMi8zKktRKtU3O1kJxtf-g3ShCRS0DAtlaBg34Byrxq8sGhSvJ8Ba_96HAvn0GfUSj8XU8ZLUlXkK0sDE4WIeCIKE_ITw3AHb8qZ8sWeJlX3_L7_P0CBG_wzpdrOmZQafe-9dTk_PvkHJu5zUnCnZ_BDfC0g9fCWUSEJSQu4_dRubV072tawpjVJQ"
+	accessToken := ""
 
 	result, err := handlers.FetchContacts(accessToken, 1)
 	if err != nil {
