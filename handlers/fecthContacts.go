1153656828cf048f03d1533f2eb27186a6729977
tmazleo <leotomazb@icloud.com>
2025-12-18 15:41:00 -0300

    update: modify fetchContacts and main

--- handlers/fecthContacts.go
+++ handlers/fecthContacts.go
@@ -9,13 +9,15 @@ import (
 
 func FetchContacts(accessToken string, page int) (map[string]interface{}, error) {
 	url := fmt.Sprintf(
-		"https://api.rd.services/platform/contacts?page=%d",
+		"https://api.rd.services/crm/v2/contacts/search?page=%d",
 		page,
 	)
 
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("Authorization", "Bearer "+accessToken)
 	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("User-Agent", "rdstation-crm-integration/1.0")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
